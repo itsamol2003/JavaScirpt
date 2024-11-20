606037410aac2d200cd496b9f465abc9acb5480b
Amol Pawar <amolpawar001213@gmail.com>
2024-11-20 12:47:39 +0530

    Initial Commit

--- day02/01_variables.js
+++ day02/01_variables.js
@@ -5,6 +5,7 @@ var email ="amolpawar001213@gmail.com"
 var password ="12345";
 var courscount =0;
 var isLoggedInFromGoogle = false;
+let accountstate;
 
 console.log(uid);
 console.log("fullname is :",fullname);
@@ -15,6 +16,7 @@ console.log(`
     User is: ${fullname}
     email is: ${email}
     password is: ${password}
-    
-    
+
  `);
+
+ console.table([ uid,fullname,email,password,courscount,isLoggedInFromGoogle,accountstate])
---
+++ day02/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all js code as  newer version
+
+//alert(3+3)
+
+console.log(3
+    +3) //code readanility should be high
+
+console.log("amol")
