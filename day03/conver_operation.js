a3cb8aceb139834e50d8a40a984f9d85a93b4669
Amol Pawar <amolpawar001213@gmail.com>
2024-11-22 19:26:43 +0530

    comparison done

--- day03/conver_operation.js
+++ day03/conver_operation.js
@@ -24,8 +24,45 @@ var booleanIsLoggedIn =Boolean(isLoggedIn)
 let somenumber =33
 
 let stringNumber = String(somenumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//************************** OPERATIONS *****************
+
+let value= 3
+let negvalue = -value
+// console.log(negvalue);
 
 
-console.log("Hello Guys");
+console.log(2+2);
+console.log(2-2);
+console.log(2%2);
+console.log(2/2);
+console.log(2*2);
+
+
+
+let str1 ="hello"
+let str2 ="amol"
+
+let str3 =str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1= num2 = num3= 2+2
+
+
+let gamecounter =100
+gamecounter++;
+console.log(gamecounter);
+
+
