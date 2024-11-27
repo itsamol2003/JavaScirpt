1480b06df04d6f30c9b3eaca6b8224d7f956ed49
Amol Pawar <amolpawar001213@gmail.com>
2024-11-27 19:27:33 +0530

    number_math

---
+++ day07/strings.js
@@ -0,0 +1,38 @@
+const name = "amol"
+const repoCount = 50
+
+console.log(Name + repoCount + "value");
+
+console.log(`Hello my name is ${Name} and my repo count is ${repoCount}`);
+
+const gameName = new String(`amol-ap`)
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf(l));
+
+const  newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherstring =gameName.slice(-8,4)
+console.log(anotherstring);
+
+const newStringOne ="  amol  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url ="https://amol.com/amol%20pawar"
+
+console.log(url.replace('%20', '_'))
+
+console,log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+
+
